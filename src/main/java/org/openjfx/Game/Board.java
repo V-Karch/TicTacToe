03b5f3d83b78208e3b14f93b26bd14aed8fa4208
V-Karch <luna.karch05@gmail.com>
2024-09-24 14:17:00 -0400

    Board: created constructor and fillBoardWithEmptyTiles() method

--- src/main/java/org/openjfx/Game/Board.java
+++ src/main/java/org/openjfx/Game/Board.java
@@ -1,5 +1,18 @@
 package org.openjfx.Game;
 
 public class Board {
+    private final Tile[][] board = new Tile[3][3];
 
+    public Board() {
+        this.fillBoardWithEmptyTiles();
+    }
+
+    private void fillBoardWithEmptyTiles() {
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board[0].length; j++) {
+                Tile tile = new Tile(i, j);
+                this.board[i][j] = tile;
+            }
+        }
+    }
 }
