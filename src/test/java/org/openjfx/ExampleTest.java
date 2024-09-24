9d5676df10902bceb5f65d93c343112efec55a0c
V-Karch <luna.karch05@gmail.com>
2024-09-24 13:47:49 -0400

    src/test: added an example test

---
+++ src/test/java/org/openjfx/ExampleTest.java
@@ -0,0 +1,12 @@
+package org.openjfx;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    void simpleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
