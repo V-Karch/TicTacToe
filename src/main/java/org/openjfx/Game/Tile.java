3304e1664d1a4491df07dfc57189e36a2f159160
V-Karch <kluna0905@gmail.com>
2024-09-24 13:53:19 -0400

    Tile: added move() stub method

--- src/main/java/org/openjfx/Game/Tile.java
+++ src/main/java/org/openjfx/Game/Tile.java
@@ -2,9 +2,20 @@ package org.openjfx.Game;
 
 public class Tile {
     public TileState state;
+    private int row;
+    private int column;
     // State can be changed arbitrarily, so it can be public
 
-    public Tile() {
+    public Tile(int row, int column) {
         this.state = TileState.NONE; // Initially a Tile will have no state
+        this.row = row;
+        this.column = column;
     }
+
+    public void move(Direction direction) {
+        // TODO: Implement
+    }
+
+    public int getRow() { return this.row; }
+    public int getColumn() { return this.column; }
 }
