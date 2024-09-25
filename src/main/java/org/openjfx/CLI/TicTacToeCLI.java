e83384314f9c2618a699dbd42d35b6f76b13227d
V-Karch <luna.karch05@gmail.com>
2024-09-25 12:51:05 -0400

    TicTacToeCLI: Added function to display the board state in cli

--- src/main/java/org/openjfx/CLI/TicTacToeCLI.java
+++ src/main/java/org/openjfx/CLI/TicTacToeCLI.java
@@ -1,7 +1,22 @@
 package org.openjfx.CLI;
 
+import org.openjfx.Game.Board;
+
 public class TicTacToeCLI {
+    private final Board board = new Board();
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        TicTacToeCLI cli = new TicTacToeCLI();
+        cli.displayBoard();
+    }
+
+    public TicTacToeCLI() {}
+
+    public void displayBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("|" + this.board.getTile(j, j).state.getValue() + "|");
+            }
+            System.out.println();
+        }
     }
 }
