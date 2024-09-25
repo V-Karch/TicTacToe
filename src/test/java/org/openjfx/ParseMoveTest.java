4f401709de2afb72dc8c22d5f566481121633423
V-Karch <kluna0905@gmail.com>
2024-09-25 17:10:20 -0400

    Added a test for parsing moves and made the method static

---
+++ src/test/java/org/openjfx/ParseMoveTest.java
@@ -0,0 +1,23 @@
+package org.openjfx;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.openjfx.CLI.TicTacToeCLI;
+
+public class ParseMoveTest {
+    @Test
+    public void parseMoveTest1_1() {
+        // Setup
+        String simulatedStdin = "1 1";
+
+        // Expected
+        int[] expected = {1, 1};
+
+        // Actual
+        int[] actual = TicTacToeCLI.parseMove(simulatedStdin);
+
+        assertEquals(expected[0], actual[0]);
+        assertEquals(expected[1], actual[1]);
+    }
+}
